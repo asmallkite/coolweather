8ecfe50ba969c31458521f6b826d5029c8f8926e
Evan <lixiaozheng.good@gmail.com>
2017-01-04 15:53:44 +0800

    change statusBar

--- app/src/main/java/cn/yibulz/coolweather/WeatherActivity.java
+++ app/src/main/java/cn/yibulz/coolweather/WeatherActivity.java
@@ -2,6 +2,8 @@ package cn.yibulz.coolweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -58,6 +60,14 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
@@ -130,6 +140,7 @@ public class WeatherActivity extends AppCompatActivity {
 
             }
         });
+        loadBingPic();
     }
 
     /**
